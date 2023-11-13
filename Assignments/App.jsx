8d7f90c1b091f7d6e45ccef9d098dbfa3344bb18
Abdul Basit <82908541+abdulbasit1234-ab@users.noreply.github.com>
2023-11-13 18:52:52 +0500

    Form Submission

--- Assignments/App.jsx
+++ Assignments/App.jsx
@@ -1,42 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { NavBar } from './nav'
 import './App.css'
+import { useState } from 'react'
+import Swal from 'sweetalert2'
 
-function FriendsListApp() {
-  const [friendName, setFriendName] = useState('');
-  const [friendsList, setFriendsList] = useState([]);
+function App() {
+  const [fname , setfName] = useState("");
+  const [lname , setlName] = useState("");
+  const [email , setemail] = useState("");
+  const [contact , setContact] = useState("");
+  const [message , setMessage] = useState("");
 
-  const handleInputChange = (event) => {
-    setFriendName(event.target.value);
-  };
-
-  const handleAddFriend = () => {
-    if (friendName.trim() !== '') {
-      setFriendsList([...friendsList, friendName]);
-      setFriendName('');
-    }
-  };
-
-  const handleClearText = () => {
-    setFriendName('');
-  };
+  const Submit = (e) =>
+  {
+    e.preventDefault();
+    Swal.fire(
+      "Form Sumitted"
+    )
+  }
 
   return (
-    <div>
-      <h1>My Friends List</h1>
-      <input type="text" placeholder="Enter friend's name" value={friendName} onChange={handleInputChange} />
-      <button onClick={handleAddFriend}>Add Friend</button>
-      <button onClick={handleClearText}>Clear</button>
-
-      <h2>Friends:</h2>
-      <ul>
-        {friendsList.map((friend, index) => (
-          <p key={index}>{friend}</p>
-        ))}
-      </ul>
-    </div>
-  );
+    <>
+      <div className='border-black h-52 w-56 bg-slate-800 mar light'>
+        <h1 className='text-center'>Contact Us</h1>
+        <form onSubmit={Submit} action="submit">
+        <input type="text" name="fname" placeholder="First Name" required="name" className='m-1 p-0 h-6 outline'/>
+        <br />
+        <input type="text" name="lname" placeholder="Last Name" required="lname" className='m-1 p-0 h-6 outline'/>
+        <br />
+        <input type="email" name="email" placeholder="Email" required="email" className='m-1 p-0 h-6 w- outline'/>
+        <br />
+        <input type="number" name="number" placeholder="Contact" required="number" className='outline m-1 p-0 h-6'/>
+        <br />
+        <input type="text" name="message" placeholder="Message" required="message" id='message' className='outline m-1 p-0 h-6'/>
+        <br />
+        <button className='outline m-1 p-0 h-6 bg-red-400'>Submit</button>
+        </form>
+      </div>
+    </>
+  )
 }
 
-export default FriendsListApp;
+export default App
