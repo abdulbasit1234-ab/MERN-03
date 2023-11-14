f500e032dd2f0779c496700525dc825d67510475
Abdul Basit <03013425446az@gmail.com.com>
2023-11-14 14:40:36 +0500

    Get Result on Submit Button

--- W4D1/myProject/src/App.jsx
+++ W4D1/myProject/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   {
     e.preventDefault();
     Swal.fire(
-      "Form Sumitted"
+      "Form Sumitted", `First Name:${fname} Last Name:${lname} Email:${email} Contact:${contact} Message:${message}`
     )
   }
 
@@ -22,15 +22,15 @@ function App() {
       <div className='border-black h-52 w-56 bg-slate-800 mar light'>
         <h1 className='text-center'>Contact Us</h1>
         <form onSubmit={Submit} action="submit">
-        <input type="text" name="fname" placeholder="First Name" required="name" className='m-1 p-0 h-6 outline'/>
+        <input type="text" onChange={(e)=>{setfName(e.target.value)}} name="fname" placeholder="First Name" required="name" className='m-1 p-0 h-6 outline'/>
         <br />
-        <input type="text" name="lname" placeholder="Last Name" required="lname" className='m-1 p-0 h-6 outline'/>
+        <input type="text" onChange={(e)=>{setlName(e.target.value)}} name="lname" placeholder="Last Name" required="lname" className='m-1 p-0 h-6 outline'/>
         <br />
-        <input type="email" name="email" placeholder="Email" required="email" className='m-1 p-0 h-6 w- outline'/>
+        <input type="email" onChange={(e)=>{setemail(e.target.value)}} name="email" placeholder="Email" required="email" className='m-1 p-0 h-6 w- outline'/>
         <br />
-        <input type="number" name="number" placeholder="Contact" required="number" className='outline m-1 p-0 h-6'/>
+        <input type="number" onChange={(e)=>{setContact(e.target.value)}} name="number" placeholder="Contact" required="number" className='outline m-1 p-0 h-6'/>
         <br />
-        <input type="text" name="message" placeholder="Message" required="message" id='message' className='outline m-1 p-0 h-6'/>
+        <input type="text" onChange={(e)=>{setMessage(e.target.value)}} name="message" placeholder="Message" required="message" id='message' className='outline m-1 p-0 h-6'/>
         <br />
         <button className='outline m-1 p-0 h-6 bg-red-400'>Submit</button>
         </form>
